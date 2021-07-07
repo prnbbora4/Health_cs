5bbb3ef06259e6d3cb0b9009c60d7153aecfd472
Pranab Bora <prnbbora4@gmail.com>
2021-07-07 22:58:09 +0530

    fgfdd

--- index.php
+++ index.php
@@ -9,19 +9,23 @@
   </head>
   <body>
     <div class="s006">
-      <form>
+      <form action="result.php" method="get">
         <fieldset>
           <legend>What are you looking for?</legend>
           <div class="inner-form">
             <div class="input-field">
-              <button class="btn-search" type="button" name="d_search">
+              
+            <button class="btn-search" type="button" name="search">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                   <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
                 </svg>
               </button>
-              <input id="search" type="text" placeholder="Enter your query" value="" />
+
+              <input name="user_query" id="search" type="text" placeholder="Press Enter to Search" value="" />
+
             </div>
           </div>
+              <input type="hidden" name="search" value="submit"/>
           <div class="suggestion-wrap">
             <span>Cancer</span>
             <span>Covid19</span>
---
+++ result.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html> 
+<html> 
+	<head> 
+		<title>Result page</title> 
+
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+		
+<style type="text/css">
+.results {margin-left:12%; margin-right:12%; margin-top:10px;}
+</style>
+	</head> 
+	
+	
+<body> 
+
+<!-- <form action="result.php" method="get"> 
+		
+		<span><b>Write your Keyword:</b></span>
+		
+		<input type="text" name="user_keyword" size="120"/> 
+		<input type="submit" name="result" value="Search Now">
+</form>
+	
+	<a href="index.php"><button>Go Back</button></a> -->
+	
+<?php 
+
+  include "navbar_result.php";
+  $upload_dir = 'Doctor/uploads/';
+
+	$con=mysqli_connect("localhost","root","");
+	mysqli_select_db($con,"hcs");
+	
+	if(isset($_GET['search'])){
+	
+	$get_value = $_GET['user_query'];
+	
+	if($get_value==''){
+	
+	echo "<center><b>Please go back, and write something in the search box!</b></center>";
+	exit();
+	}
+	
+	$result_query = "SELECT * FROM approved WHERE speciali LIKE '%$get_value%'";
+	
+	$run_result = mysqli_query($con,$result_query);
+	
+	if(mysqli_num_rows($run_result)<1){
+	
+	echo "<center><b>Oops! sorry, nothing was found in the database!</b></center>";
+	exit();
+	
+	}
+	
+	while($row_result=mysqli_fetch_array($run_result)){
+		
+		$qualifi=$row_result['qualifi'];
+		$speciali=$row_result['speciali'];
+		$experience=$row_result['experience'];
+		$symptoms=$row_result['symptoms'];
+    $image=$row_result['image'];
+
+	
+	echo "<div class='card mb-3 container my-5' style='max-width: 540px;'>
+    <div class='row g-0'>
+      <div class='col-md-4'>
+        <img src='$upload_dir$image' class='img-fluid rounded-start' alt='...'>
+      </div>
+      <div class='col-md-8'>
+        <div class='card-body'>
+          <h5 class='card-title'>$qualifi</h5>
+          <p class='card-text'>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class='card-text'><small class='text-muted'>$symptoms</small></p>
+
+        </div>
+      </div>
+    </div>
+  </div>";
+
+		}
+}
+
+
+?>
+
+</body> 
+</html>
+
+
+
+<!-- <div class='container card text-center my-3'>
+    <div class='card-header'>
+      Doctor details
+    </div>
+    <div class='card-body'>
+      <h5 class='card-title'>$qualifi</h5>
+      <p class='card-text'>$symptoms</p>
+      <a href='#' class='btn btn-primary'>View</a>
+    </div>
+    <div class='card-footer text-muted'>
+    </div>
+  </div><br> -->
